812e81c2dc871df9d33d63c48a58427c1ff06fec
肖伟 <1097526615@qq.com>
2018-07-01 11:46:55 +0800

    Add framework

---
+++ XWPrint/XWPrint/XWPrint.swift
@@ -0,0 +1,38 @@
+//
+//  XWPrint.swift
+//  XWPrint
+//
+//  Created by Metallic  on 2018/7/1.
+//  Copyright © 2018年 xiaowei. All rights reserved.
+//
+
+import Foundation
+
+public class XWPrint {
+    
+    public private(set) var capitalized: Bool 
+    
+    public init(with capitalized: Bool) {
+        self.capitalized = capitalized
+    }
+    
+    public func sayHello() {
+        sayText("Hello World!")
+    }
+    
+    public func sayText(_ text: String) {
+        var printText = text
+        if capitalized {
+            printText = printText.capitalized
+        }
+        print(printText)
+    }
+    
+    public class func log(with info: String, params: Any...) {
+        var logString = info
+        for param in params {
+            logString.append("\(param) ")
+        }
+        print(logString)
+    }
+}
---
+++ XWPrint/XWPrintKit/XWPrintKit.h
@@ -0,0 +1,19 @@
+//
+//  XWPrintKit.h
+//  XWPrintKit
+//
+//  Created by Metallic  on 2018/7/1.
+//  Copyright © 2018年 xiaowei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for XWPrintKit.
+FOUNDATION_EXPORT double XWPrintKitVersionNumber;
+
+//! Project version string for XWPrintKit.
+FOUNDATION_EXPORT const unsigned char XWPrintKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <XWPrintKit/PublicHeader.h>
+
+
